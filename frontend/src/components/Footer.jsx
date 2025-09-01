df51cded52dd745440e35596200efc7cdd5ec457
Ritesh2006M <riteshcode12@gmail.com>
2025-09-01 12:50:06 +0530

    Added tailwind

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-900 text-white py-4 text-center">
+      <p className="text-sm">&copy; 2025 SciFusion</p>
+    </footer>
+  );
+}
